ffc2086b9b64d391ee19ed97f200c3f6fc83e56e
Hugh Graham <43027949+h-a-graham@users.noreply.github.com>
2019-10-08 16:30:38 +0100

    Update GB_BVI_master.py

--- SI8_BDC_BFI_Python_code/Beaver_HabVeg_Index/GB_BVI_master.py
+++ SI8_BDC_BFI_Python_code/Beaver_HabVeg_Index/GB_BVI_master.py
@@ -1,10 +1,11 @@
-# Exampl wrapper for running BFI tools. This script will run all necessary commands to generate BFI for GB from raw data inputs.
+# Example wrapper for running BFI tools. This script will run all necessary commands to generate BFI for GB from raw data inputs.
+
 import os
 import OS_Vector_PP
 import TCD_Map_PrePro
 import CEH_LCM_PrePro
 import LWF_CEH_PP_arcpy
-# import Convert_MM_rivers
+import Convert_MM_rivers
 import Water_Area_Buffer
 import merge_resamp_BVI
 import BHI_Exports
@@ -12,16 +13,16 @@ import GB_BVI
 
 def main():
     #working directories
-    scratch = os.path.abspath("C:/Users/hughg/Desktop/GB_Beaver_modelling/BVI_scratch")
-    exports = os.path.abspath("D:/Work/GB_Beaver_Data/GB_BVI_Res_v2")
+    scratch = os.path.abspath("C:/...")
+    exports = os.path.abspath("D:/...")
 
     # data directories
-    OrdSurv_fold = os.path.abspath("D:/Work/GB_Beaver_Data/Edina/OS_Vector")
-    Ordnan_Grid = os.path.abspath("C:/Users/hughg/Desktop/GB_Beaver_modelling/OS_Grids/100km_grid_region.shp")
-    tcd_cop_fold = "C:/Users/hughg/Desktop/GB_Beaver_modelling/Raw_Data/TCD_Data"
-    LCM_data = os.path.abspath("D:/Work/GB_Beaver_Data/Data/lcm-2015-vec_1904131/LCM2015_GB.gdb/LCM2015GBvector")
-    LWF_data = os.path.abspath("C:/Users/hughg/Desktop/GB_Beaver_modelling/Raw_Data/GB_WLF_V1_0.gdb/GB_WLF_V1_0")
-    rivers_folder = os.path.abspath("C:/Users/hughg/Desktop/GB_Beaver_modelling/Raw_Data/mastermap-water/2018_10/gml")
+    OrdSurv_fold = os.path.abspath("D:/...")
+    Ordnan_Grid = os.path.abspath("C:/.../100km_grid_region.shp")
+    tcd_cop_fold = "C:/..."
+    LCM_data = os.path.abspath("D:/...")
+    LWF_data = os.path.abspath("C:/...")
+    rivers_folder = os.path.abspath(".../gml")
 
     #project variables
     epsg_code = str(27700)

27be894ab46525a0ea9d56939139189520ff6ba3
Hugh Graham <43027949+h-a-graham@users.noreply.github.com>
2019-10-08 16:19:57 +0100

    Update GB_BVI_master.py

--- SI8_BDC_BFI_Python_code/Beaver_HabVeg_Index/GB_BVI_master.py
+++ SI8_BDC_BFI_Python_code/Beaver_HabVeg_Index/GB_BVI_master.py
@@ -1,4 +1,4 @@
-# This script will run all necessary commands to generate BVI for GB from raw data inputs.
+# Exampl wrapper for running BFI tools. This script will run all necessary commands to generate BFI for GB from raw data inputs.
 import os
 import OS_Vector_PP
 import TCD_Map_PrePro
@@ -27,20 +27,20 @@ def main():
     epsg_code = str(27700)
     buff_size = 100
 
-    # OS_Vector_PP.OS_Vec_main(epsg_code, OrdSurv_fold, exports, Ordnan_Grid)
-    #
-    # TCD_Map_PrePro.tcd_main(scratch, exports, tcd_cop_fold, Ordnan_Grid, epsg_code)
-    #
-    # CEH_LCM_PrePro.lcm_main(epsg_code, LCM_data, Ordnan_Grid, scratch, exports) # Now working.
-    #
-    # LWF_CEH_PP_arcpy.lwf_main(epsg_code, LWF_data, Ordnan_Grid, exports, scratch)
+    OS_Vector_PP.OS_Vec_main(epsg_code, OrdSurv_fold, exports, Ordnan_Grid)
+    
+    TCD_Map_PrePro.tcd_main(scratch, exports, tcd_cop_fold, Ordnan_Grid, epsg_code)
+    
+    CEH_LCM_PrePro.lcm_main(epsg_code, LCM_data, Ordnan_Grid, scratch, exports) # Now working.
+    
+    LWF_CEH_PP_arcpy.lwf_main(epsg_code, LWF_data, Ordnan_Grid, exports, scratch)
 
-    # Convert_MM_rivers.MM_conv_main(riv_line_fold, scratch) # bit complex and long best to run in advance...
+    Convert_MM_rivers.MM_conv_main(riv_line_fold, scratch) # bit complex and long best to run in advance...
 
-    # Water_Area_Buffer.riv_area_main(buff_size, epsg_code, rivers_folder, scratch, exports, Ordnan_Grid)
+    Water_Area_Buffer.riv_area_main(buff_size, epsg_code, rivers_folder, scratch, exports, Ordnan_Grid)
 
-    # GB_BVI.main(exports)   #
-    # merge_resamp_BVI.mer_res_main(exports, epsg_code)
+    GB_BVI.main(exports)   #
+    merge_resamp_BVI.mer_res_main(exports, epsg_code)
     BHI_Exports.exports_main(exports)
 
 
